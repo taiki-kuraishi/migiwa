deb4790db183d71c217f5e7a03b7978f2f00391c
taiki-kuraishi <131664944+taiki-kuraishi@users.noreply.github.com>
2026-09-04 01:43:51 +0000

    🔥 remove: drop apps, packages, old CI and docs for the rebuild

--- packages/db/src/schemas/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./activity-sessions";
-export * from "./end-reasons";
-export * from "./events";
-export * from "./guilds";
-export * from "./presence-sessions";
-export * from "./voice-sessions";
